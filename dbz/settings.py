8443240e78061c563f8170b6caf703da3cfc1170
Alexander Zaak <alexander.zaak@gmail.com>
2018-12-14 21:43:42 +0100

    migrate to postgresql

--- dbz/settings.py
+++ dbz/settings.py
@@ -59,25 +59,11 @@ WSGI_APPLICATION = 'dbz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbz',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
-}
-'''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+    
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
