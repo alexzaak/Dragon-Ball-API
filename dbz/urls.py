6222af090ca5d3f1f2d567fe05c80fa4addd89b0
Alexander Zaak <alexander.zaak@gmail.com>
2018-12-15 10:05:17 +0100

    changed to list

--- dbz/urls.py
+++ dbz/urls.py
@@ -5,7 +5,7 @@ import fusion.views
 import sagas.views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', sagas.views.index),
     url(r'^type_character/$', character.views.get_all_types),
     url(r'^type_character/(?P<type_character_id>\d+)/', character.views.get_type),
@@ -16,4 +16,4 @@ urlpatterns = patterns('',
     url(r'^fusion/$', fusion.views.get_all_fusions),
     url(r'^fusion/(?P<name_or_id>\d+)', fusion.views.get_fusion),
     url(r'^seeds/$', character.views.seeds)
-)
+]
