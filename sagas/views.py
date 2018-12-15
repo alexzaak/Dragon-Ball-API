b32d4b012d20e9d4ae6479e4e01aa8ed63fd574f
Alexander Zaak <alexander.zaak@gmail.com>
2018-12-15 11:48:57 +0100

    fix param for saga

--- sagas/views.py
+++ sagas/views.py
@@ -26,8 +26,7 @@ def get_all_sagas(request):
 
 def get_saga(request, id_saga):
     historia = saga.objects.get(id=int(id_saga))
-    saga_id = historia.id
-    personagens = character.objects.get(saga_id=historia.id)
+    personagens = character.objects.get(saga_id_id=int(historia.id)
     sag = {}
     allcharacter = []
     for carinhas in personagens:
